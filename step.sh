ebb61598301b10a5d534c37014b5ee0d27833e45
Daniel Jette <daniel.jette@neofinancial.com>
2021-09-27 16:23:20 -0400

    Update script for first test run

--- step.sh
+++ step.sh
@@ -1,22 +1,130 @@
 #!/bin/bash
-set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+RESTORE='\033[0m'
+RED='\033[00;31m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;36m'
+GREEN='\033[00;32m'
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+error()
+{
+    echo -e "${RED}[ERROR] ${1}${RESTORE}" >&2
+}
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+warn()
+{
+    echo -e "${YELLOW}[WARNING] ${1} ${RESTORE}" >&2
+}
+
+info()
+{
+    echo -e "${BLUE}[INFO]${RESTORE} ${1}" >&2
+}
+
+output_state()
+{
+    info "Bash version: ${BASH_VERSION}"
+    info "jq version:   $(jq --version)"
+    info "Current path: $(pwd)"
+    info "artifact_file: $artifact_file"
+    info "download_dir: $download_dir"
+    info "app_slug: $app_slug"
+}
+
+
+validate_input()
+{
+    if [ -z "$artifact_file" ]; then
+        error "Please specify the file name of the artifact to download as the argument 'artifact_file'"
+        exit 1
+    fi
+
+    if [ -z "$download_dir" ]; then
+        error "Please specify the download directory as the argument 'download_dir'"
+        exit 1
+    fi
+
+    if [ -z "$app_slug" ]; then
+        error "Please define app_slug"
+        exit 1
+    fi
+
+    if [ -z "$access_token" ]; then
+        error "Please define BITRISE_KEY"
+        exit 1
+    fi
+
+    if [ -z "$source_build_slug" ]; then
+        error "Please define source_build_slug"
+        exit 1
+    fi
+}
+
+fetch_artifact_manifest()
+{
+    URL="https://api.bitrise.io/v0.1/apps/$app_slug/builds/$source_build_slug/artifacts"
+    local JSON=$(curl -s -X GET $URL -H "accept: application/json" -H "Authorization: $access_token")
+    echo $JSON > manifest.json
+}
+
+get_total_item_count()
+{
+    echo $(cat manifest.json | jq ".paging .total_item_count")
+}
+
+get_artifact_url()
+{
+    SLUG=${1//\"/}
+    URL="https://api.bitrise.io/v0.1/apps/$app_slug/builds/$source_build_slug/artifacts/$SLUG"
+    local JSON=$(curl -s -X GET $URL -H "accept: application/json" -H "Authorization: $access_token")
+    echo $JSON > file.info.json
+    local retval=$(cat file.info.json | jq ".data .expiring_download_url")
+    echo $retval
+}
+
+download_artifact()
+{
+    info "Downloading $artifact_file to $DOWNLOAD_PATH/$artifact_file"
+
+    TOTAL_ITEM_COUNT=$(get_total_item_count)
+    if [[ "$TOTAL_ITEM_COUNT" == "0" ]]; then
+        error "No artifacts to download found"
+        exit 1
+    fi
+
+    END=$(expr $TOTAL_ITEM_COUNT)
+    for ((i=0;i<END;i++));
+    do
+       slug=$(cat manifest.json | jq ".data[$i] .slug")
+       title=$(cat manifest.json | jq ".data[$i] .title")
+       title=${title//\"/}
+
+       if [[ $title == *"$artifact_file"* ]]; then
+         url=$(get_artifact_url $slug)
+         url=${url//\"/}
+         curl -s $url -o "$DOWNLOAD_PATH/$title"
+       fi
+    done
+}
+
+clean_up()
+{
+    rm -f manifest.json
+    rm -f file.info.json
+}
+
+main() {
+    output_state
+    validate_input
+
+    DOWNLOAD_PATH="$BITRISE_SOURCE_DIR/$download_dir"
+    mkdir -p $DOWNLOAD_PATH
+
+    fetch_artifact_manifest
+    download_artifact
+    clean_up
+
+    exit 0
+}
+
+main
